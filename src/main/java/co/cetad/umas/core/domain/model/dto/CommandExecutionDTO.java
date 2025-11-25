4a9955458cf8fba12ee82c7b3c1f55e5c131c258
Cristian Osia <osia.developer@gmail.com>
2025-11-24 23:12:24 -0500

    Se agrega la entidad de ruta kml

--- src/main/java/co/cetad/umas/core/domain/model/dto/CommandExecutionDTO.java
+++ src/main/java/co/cetad/umas/core/domain/model/dto/CommandExecutionDTO.java
@@ -1,13 +1,18 @@
 package co.cetad.umas.core.domain.model.dto;
 
+import co.cetad.umas.core.domain.model.vo.Waypoint;
 import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
 import java.util.Map;
 
 public record CommandExecutionDTO(
         @JsonProperty("vehicleId") String vehicleId,
         @JsonProperty("commandCode") String commandCode,
         @JsonProperty("arguments") Map<String, Double> arguments,
-        @JsonProperty("priority") Integer priority
+        @JsonProperty("priority") Integer priority,
+        @JsonProperty("missionId") String missionId,
+        @JsonProperty("waypoints") List<Waypoint> waypoints
 ) {
 
     public CommandExecutionDTO {
